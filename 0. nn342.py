7fa3ce937aad12675ef72766ea8d87606114630b
Hua Meng <hmeng22@users.noreply.github.com>
2018-08-24 11:46:30 -0400

    Update and rename nn342.py to 0. nn342.py

--- 0. nn342.py
+++ 0. nn342.py
@@ -1,3 +1,8 @@
+# Naive Neural Network
+# nn342 using tensorflow.
+
+import tensorflow as tf
+
 # I % 2 = O1*2 + O2
 # I A B C O1 O2
 # 0 0 0 0  0  0
@@ -9,28 +14,31 @@
 # 6 1 1 0  3  0
 # 7 1 1 1  3  1
 
-import tensorflow as tf
-
 train_x = [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1], [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]]
 train_y = [[0, 0], [0, 1], [1, 0], [1, 1], [2, 0], [2, 1], [3, 0], [3, 1]]
 
+# Layer 1 : Input Layer
 X = tf.placeholder(tf.float32, [8, 3], name='InputData')
 Y = tf.placeholder(tf.float32, [8, 2], name='LabelData')
 
+# Initialize parameters
 weights_34 = tf.Variable(tf.random_normal([3, 4]), name='Weights_34')
 weights_42 = tf.Variable(tf.random_normal([4, 2]), name='Weights_42')
 
 biases_34 = tf.Variable(tf.random_normal([1, 4]), name='Biases_34')
 biases_42 = tf.Variable(tf.random_normal([1, 2]), name='Biases_42')
 
+# Layer 2 : Hidden Layer
 with tf.name_scope('Layer_34'):
     layer_34 = tf.add(tf.matmul(X, weights_34), biases_34)
     layer_34_sigmoid = tf.nn.sigmoid(layer_34)
 
+# Layer 3 : Output Layer
 with tf.name_scope('Layer_42'):
     layer_42 = tf.add(tf.matmul(layer_34_sigmoid, weights_42), biases_42)
     layer_42_output = tf.nn.relu(layer_42)
 
+# Loss & GradientDescent
 with tf.name_scope('Loss'):
     cost = tf.square(Y - layer_42_output)
 
